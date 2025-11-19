7c6d876d258de4a257ea3b39676b7739afa0caca
Noah Pinales <pinales@usc.edu>
2025-11-18 16:14:53 -0800

    consolidate charts

--- src/lib/components/SportsGraphs.svelte
+++ src/lib/components/SportsGraphs.svelte
@@ -8,35 +8,52 @@
 		LineController,
 		LineElement,
 		PointElement,
+		Colors,
+		Legend
 	} from "chart.js";
 	import { onMount } from "svelte";
-	let { data }: {data: {id: number, facilityName: string, locationName: string, capacity: number, counts: number[], times: string[]}} = $props();
-
+	let { data }: {data: {id: number, facilityName: string,datasets:{ label: string, data: number[]}[], times: string[]}} = $props();
+	console.log(data.datasets)
 
 	Chart.register([
 		CategoryScale,
 		LineController,
 		LineElement,
 		LinearScale,
+		Colors,
 		PointElement,
 	]);
 
 	let cnv: HTMLCanvasElement;
 	onMount(async () => {
+	let datas: any = [];
+	data.datasets.forEach((dataset, i) => {
+		datas.push({
+		label: dataset.label,
+		data: dataset.data,
+		})
+		console.log(dataset.data.length, data.times.length)
+	})
 		const chart = new Chart(
 			cnv, // TypeScript needs "as any" here
 			{
 				type: "line",
 				data: {
 					labels: data.times,
-					datasets: [
-						{
-							label: "# of Votes",
-							data: data.counts,
-							borderColor: "red",
-						},
-					],
+					datasets: datas,
 				},
+				options: {
+				  interaction: {
+                    intersect: false,
+                    mode: 'nearest',
+                    axis: "x"
+                  },
+                  plugins: {
+                    title: {
+                      display: true,
+                    },
+                  }
+				}
 			}
 		);
 	});
--- src/routes/(admin)/admin/temp_sports/+page.svelte
+++ src/routes/(admin)/admin/temp_sports/+page.svelte
@@ -1,63 +1,111 @@
 <script lang="ts">
-	import SportsGraphs from "$lib/components/SportsGraphs.svelte";
-	import type { NumericRange } from "@sveltejs/kit";
-	import {
-		CategoryScale,
-		Chart,
-		LinearScale,
-		LineController,
-		LineElement,
-		PointElement,
-	} from "chart.js";
-	import { onMount } from "svelte";
-	let { data } = $props();
+    import SportsGraphs from "$lib/components/SportsGraphs.svelte";
+    import type { NumericRange } from "@sveltejs/kit";
+    import {
+        CategoryScale,
+        Chart,
+        LinearScale,
+        LineController,
+        LineElement,
+        PointElement,
+        Tooltip,
+        Legend
+    } from "chart.js";
+    import { onMount } from "svelte";
+    let { data } = $props();
 
-	let { session, supabase } = data;
+    let { session, supabase } = data;
 
-	Chart.register([
-		CategoryScale,
-		LineController,
-		LineElement,
-		LinearScale,
-		PointElement,
-	]);
+    Chart.register([
+        CategoryScale,
+        LineController,
+        LineElement,
+        LinearScale,
+        PointElement,
+        Tooltip,
+        Legend,
+    ]);
 
-	let cnv: HTMLCanvasElement;
+    let cnv: HTMLCanvasElement;
 
-    let facilities: {id: number, facilityName: string, locationName: string, capacity: number, counts: number[], times: string[]}[] = $state.raw([]);
+    let facilities: {
+        id: number;
+        facilityName: string;
+        datasets: { label: string; id: number; data: number[] }[];
+        times: string[];
+    }[] = $state.raw([]);
 
-	onMount(async () => {
-        let {data, error} = await supabase.from("sports_facility_data").select("*");
-        processData(data)
-	});
+    onMount(async () => {
+        let { data, error } = await supabase
+            .from("sports_facility_data")
+            .select("*");
+        processData(data);
+    });
+    let times: string[] = [];
 
     function processData(data: any) {
-        //data is listed 
+        //data is listed
         data.forEach((packet: any) => {
+            let date = new Date(packet.created_at).toLocaleString("en-US", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            times.push(date);
+
             packet.data.forEach((dat: any) => {
-                let id = dat.LocationId;
-                let idx = facilities.findIndex((d)=>{return d.id == id})
-                if(idx == -1) {
-                    facilities = [...facilities, {
-                        id,
-                        facilityName: dat.FacilityName,
-                        locationName: dat.LocationName,
-                        capacity: dat.TotalCapacity,
-                        counts: [],
-                        times: []
-                    }]
-                    idx = facilities.findIndex((d)=>{return d.id == id})
+                let id = dat.FacilityId;
+                let idx = facilities.findIndex((d) => {
+                    return d.id == id;
+                });
+                if (idx == -1) {
+                    facilities = [
+                        ...facilities,
+                        {
+                            id,
+                            facilityName: dat.FacilityName,
+                            datasets: [],
+                            times: [],
+                        },
+                    ];
+                    idx = facilities.findIndex((d) => {
+                        return d.id == id;
+                    });
                 }
-                facilities[idx].counts = [...facilities[idx].counts, dat.LastCount]
-                facilities[idx].times = [...facilities[idx].times, dat.LastUpdatedDateAndTime]
-                
+                let Lid = dat.LocationId;
+                let idx2 = facilities[idx].datasets.findIndex((d) => {
+                    return d.id == Lid;
+                });
+                if (idx2 == -1) {
+                    facilities[idx].datasets = [
+                        ...facilities[idx].datasets,
+                        {
+                            label: dat.LocationName,
+                            id: Lid,
+                            data: [],
+                        },
+                    ];
+                    idx2 = facilities[idx].datasets.findIndex((d) => {
+                        return d.id == Lid;
+                    });
+                }
+                facilities[idx].datasets[idx2].data = [
+                    ...facilities[idx].datasets[idx2].data,
+                    dat.LastCount,
+                ];
+                // facilities[idx].times = [...facilities[idx].times, dat.LastUpdatedDateAndTime]
             });
         });
 
-        console.log(facilities)
+        for (let i = 0; i < facilities.length; i++) {
+            facilities[i].times = [...times];
+        }
     }
 </script>
 
 {#each facilities as facility}
+    {facility.facilityName}
     <SportsGraphs data={facility}></SportsGraphs>
 {/each}
